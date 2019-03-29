f7853d19fd23d443a2a291a78b8a7c528244e464
visualstudio <visualstudio@122-PC38>
2019-03-29 19:59:03 -0400

    7:58

---
+++ c0732132/C0732132/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C0732132
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading a file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+        }
+
+    }
+}
+
